554e03790ef134344a9ebd800807423e8879f0ad
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-11-06 12:38:35 +0600

    add Admin Header responsive

--- Front End/src/Components/Views/Auth/Admin/AdminHeader/AdminHeader.jsx
+++ Front End/src/Components/Views/Auth/Admin/AdminHeader/AdminHeader.jsx
@@ -6,11 +6,11 @@ const AdminHeader = () => {
         <div className="flex justify-between items-center p-4 bg-[#F6D4BA] fixed top-0 w-full">
             <div className="ml-0 lg:ml-[260px] rounded-md h-[48px] flex items-center transition-all w-full">
 
-                <div className="flex-1 hidden md:block">
+                <div className="flex-1 ">
                     <SearchBar />
                 </div>
 
-                <div className=" items-center hidden md:block">
+                <div className=" items-center ">
                     <Profile />
                 </div>
 
--- Front End/src/Components/Views/Auth/Admin/AdminHeader/SearchBar.jsx
+++ Front End/src/Components/Views/Auth/Admin/AdminHeader/SearchBar.jsx
@@ -3,11 +3,11 @@ import { FaSearch } from 'react-icons/fa';
 
 const SearchBar = () => {
     return (
-        <div className="relative w-full max-w-[400px]">
+        <div className="relative w-full max-w-[200px] md:max-w-[400px] lg:max-w-[600px] ">
             <input
                 type="text"
                 name="search"
-                placeholder="Search for anything..."
+                placeholder="Search..."
                 className="w-full py-2 pl-12 pr-4 bg-gradient-to-r from-[#ffe4e4] to-[#ffded2] text-gray-700 rounded-full border-2 border-transparent outline-none shadow-md focus:border-[#ff6b4a] focus:shadow-lg transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-[#ffd7d7] hover:to-[#ffd0c8]"
             />
             <FaSearch className="absolute left-4 top-1/2 transform -translate-y-1/2 text-[#d6785b] text-lg pointer-events-none" />
